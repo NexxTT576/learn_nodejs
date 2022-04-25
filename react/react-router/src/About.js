7ae1b826c752b3039e9abc86e44acc1e57576ca6
metoo5566 <351757417@qq.com>
2022-04-26 00:36:39 +0800

    跳转传参

--- react/react-router/src/About.js
+++ react/react-router/src/About.js
@@ -1,4 +1,15 @@
-function About(params) {
-  return <div>About</div>;
+import { useSearchParams } from "react-router-dom";
+function About() {
+  const [params] = useSearchParams();
+  // params是一个对象 对象里有一个get的方法
+  // 用来获取对应的参数
+  // 把参数的名称作为get方法的实参传过来
+  const id = params.get("id");
+  const name = params.get("name");
+  return (
+    <div>
+      About 得到的参数id值为{id} name为{name}
+    </div>
+  );
 }
 export default About;
--- react/react-router/src/App.js
+++ react/react-router/src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
-
+import {
+  // HashRouter,
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+// 进行路由配置
 function App() {
-  return <div className="App"></div>;
+  return (
+    // 声明当前要用一个非hash模式的路由
+    <BrowserRouter>
+      {/**指定跳转的组件 to用来配置路由地址 */}
+      <Link to="/">首页</Link>
+      <Link to="/about">关于</Link>
+      {/**路由出口 路由对应的组件会在这里进行渲染 */}
+      <Routes>
+        {/**指定路径和组件的对应关系 path代表路径 element代表组件 成对出现 path->element */}
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
