73f0639489f7992638f5009b226b51e771a9da07
metoo5566 <351757417@qq.com>
2022-04-30 19:30:33 +0800

    mobx todomvc

--- react/mobx_react/src/Todo/index.js
+++ react/mobx_react/src/Todo/index.js
@@ -1,46 +1,97 @@
-import './index.css'
+import { observer } from "mobx-react-lite";
+import { useState } from "react";
+import { useStore } from "../store";
+import "./index.css";
+import uuid from "react-uuid";
 function Task() {
+  const { taskStore } = useStore();
+  // 单选受控
+  // 思想：mobx Store 去维护状态 input只需要把e.target.value 交给store让它来进行修改
+  function onChange(e, id) {
+    console.log(e.target.value, id);
+    taskStore.singleCheck(id, e.target.checked);
+  }
+  // 全选
+  function allChange(e) {
+    console.log(e);
+    taskStore.allCheck(e.target.checked);
+  }
+  // 删除
+  function delTask(id) {
+    console.log(id);
+    taskStore.delTask(id);
+  }
+  // 新增
+  const [taskValue, setTaskValue] = useState("");
+  function addTask(e) {
+    if (e.keyCode === 13) {
+      taskStore.addTask({
+        id: uuid(),
+        name: taskValue,
+        idDone: false,
+      });
+      setTaskValue("");
+    }
+  }
   return (
     <section className="todoapp">
       <header className="header">
         <h1>todos</h1>
+        {/**新增输入框 */}
         <input
           className="new-todo"
           autoFocus
           autoComplete="off"
+          value={taskValue}
+          onChange={(e) => setTaskValue(e.target.value)}
+          onKeyUp={addTask}
           placeholder="What needs to be done?"
         />
       </header>
       <section className="main">
+        {/**全选 */}
         <input
           id="toggle-all"
           className="toggle-all"
           type="checkbox"
+          checked={taskStore.isAll}
+          onChange={allChange}
         />
         <label htmlFor="toggle-all"></label>
         <ul className="todo-list">
-          <li
-            className="todo"
-          >
-            <div className="view">
-              <input className="toggle" type="checkbox" />
-              <label >learn react</label>
-              <button className="destroy"></button>
-            </div>
-          </li>
-          <li
-            className="todo completed"
-          >
-            <div className="view">
-              <input className="toggle" type="checkbox" defaultChecked={true}/>
-              <label >learn react</label>
-              <button className="destroy"></button>
-            </div>
-          </li>
+          {/**completed 类名标识 */}
+          {taskStore.list.map((item) => (
+            <li
+              className={item.isDone ? "todo completed" : "todo"}
+              key={item.id}
+            >
+              <div className="view">
+                {/**单选框 受控和非受控 受控的方式 */}
+                <input
+                  className="toggle"
+                  type="checkbox"
+                  // defaultChecked={item.isDone}
+                  onChange={(e) => onChange(e, item.id)}
+                  checked={item.isDone}
+                />
+                <label>{item.name}</label>
+                <button
+                  className="destroy"
+                  onClick={() => delTask(item.id)}
+                ></button>
+              </div>
+            </li>
+          ))}
         </ul>
       </section>
+      <footer className="footer">
+        <span className="todo-count">
+          任务总数：{taskStore.list.length} 已完成数：
+          {taskStore.isFinishedLength()}
+        </span>
+      </footer>
     </section>
-  )
+  );
 }
 
-export default Task
+export default observer(Task);
--- react/mobx_react/src/store/task.Store.js
+++ react/mobx_react/src/store/task.Store.js
@@ -1,20 +1,49 @@
-
-import {  makeAutoObservable } from 'mobx'
+import { makeAutoObservable } from "mobx";
 class TaskStore {
   list = [
     {
-      id:1,
-      name: '学习react',
-      isDone: true
+      id: 1,
+      name: "学习react",
+      isDone: true,
     },
     {
-      id:2,
-      name: '搞定mobx',
-      isDone: true
-    }
-  ]
+      id: 2,
+      name: "搞定mobx",
+      isDone: true,
+    },
+  ];
   constructor() {
-    makeAutoObservable(this)
+    makeAutoObservable(this);
   }
+  // 单选操作
+  singleCheck(id, check) {
+    //查找
+    const item = this.list.find((item) => item.id === id);
+    item.isDone = check;
+  }
+  // 全选操作
+  allCheck(checked) {
+    // 把所有项的isDone属性 都按照传入的最新的状态修改
+    this.list.forEach((item) => {
+      item.isDone = checked;
+    });
+  }
+  // 计算属性 只有所有子项都是选中的时候 才是选中的状态
+  get isAll() {
+    return this.list.every((item) => item.isDone);
+  }
+  isFinishedLength() {
+    return this.list.filter((item) => item.isDone).length;
+  }
+  // 删除
+  delTask = (id) => {
+    // this.list = this.list.filter((item) => item.id !== id);
+    const index = this.list.findIndex((item) => item.id === id);
+    this.list.splice(index, 1);
+  };
+  // 新增
+  addTask = (task) => {
+    this.list.push(task);
+  };
 }
-export default TaskStore
+export default TaskStore;
